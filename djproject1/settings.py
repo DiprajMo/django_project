4288427cf3487424475b7ff6cffcb81b1ca6ab45
DiprajMo <2023_dipraj.moktan@iticbcn.cat>
2026-02-02 17:10:14 +0100

    nova app

--- djproject1/settings.py
+++ djproject1/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-4d)+gnl$8n1tyo&c8_7s9ytr&llj=fxnp2#x09&%2_j7m%6#^_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'djproject1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
